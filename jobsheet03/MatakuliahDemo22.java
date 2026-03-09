e26a9adca53d1853eb49d175068029a33217345a
rizdannn <rafifprastana@gmail.com>
2026-03-09 21:27:20 +0700

    3.2 Membuat Array dari Object, Mengisi dan Menampilkan

---
+++ jobsheet03/MatakuliahDemo22.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class MatakuliahDemo22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String kode, nama, dummy;
+        int sks, jumlahJam;
+
+        System.out.print("Masukkan jumlah Matakuliah : ");
+        int jumlah = Integer.parseInt(sc.nextLine());
+
+        Matakuliah22[] arrayOfMatakuliah = new Matakuliah22[jumlah];
+
+        for(int i=0; i < jumlah; i++) {
+            arrayOfMatakuliah[i] = new Matakuliah22("-", "-", 0, 0);
+
+            System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
+            System.out.print("Kode       : ");
+            kode = sc.nextLine();
+            System.out.print("Nama       : ");
+            nama = sc.nextLine();
+            System.out.print("Sks        : ");
+            dummy = sc.nextLine();
+            sks = Integer.parseInt(dummy);
+            System.out.print("Jumlah Jam : ");
+            dummy = sc.nextLine();
+            jumlahJam = Integer.parseInt(dummy);
+            System.out.println("------------------------------");
+
+            arrayOfMatakuliah[i].tambahData(kode, nama, sks, jumlahJam);
+        }
+
+        for(int i=0; i < jumlah; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i].cetakInfo();
+        }
+    }
+}
