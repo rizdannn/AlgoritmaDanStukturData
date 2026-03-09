f737e5e52dbe063aa11e9f28a6ece87ec42b075c
rizdannn <rafifprastana@gmail.com>
2026-03-09 21:27:30 +0700

    3.2 Membuat Array dari Object, Mengisi dan Menampilkan

---
+++ jobsheet03/Matakuliah22.java
@@ -0,0 +1,29 @@
+public class Matakuliah22 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public Matakuliah22(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("Sks        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("------------------------------");
+    }
+
+}
