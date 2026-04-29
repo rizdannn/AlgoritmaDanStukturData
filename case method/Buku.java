cfba787a9f8e0b6c5d4b3957f879ef20b69e48fc
rizdannn <rafifprastana@gmail.com>
2026-04-29 08:51:12 +0700

    Class Buku

---
+++ case method/Buku.java
@@ -0,0 +1,15 @@
+public class Buku {
+    String kodeBuku;
+    String judul;
+    int tahunTerbit;
+ 
+    public Buku(String kodeBuku, String judul, int tahunTerbit) {
+        this.kodeBuku = kodeBuku;
+        this.judul = judul;
+        this.tahunTerbit = tahunTerbit;
+    }
+ 
+    public void tampilBuku() {
+        System.out.println("Kode: " + kodeBuku + " | Judul: " + judul + " | Tahun: " + tahunTerbit);
+    }
+}
