113eeb70714ae37045ed104d3bb1d1caf4b28710
rizdannn <rafifprastana@gmail.com>
2026-05-06 20:16:21 +0700

    Class Surat

---
+++ jobsheet09/Surat22.java
@@ -0,0 +1,20 @@
+public class Surat22 {
+
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char   jenisIzin;
+    int    durasi;
+
+    public Surat22() {}
+
+    public Surat22(String idSurat, String namaMahasiswa,
+                 String kelas, char jenisIzin, int durasi) {
+        this.idSurat       = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas         = kelas;
+        this.jenisIzin     = jenisIzin;
+        this.durasi        = durasi;
+    }
+
+}
