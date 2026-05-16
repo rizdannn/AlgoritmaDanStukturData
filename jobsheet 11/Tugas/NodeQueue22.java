e7a157dc7f25992e26f228380791a2fb079b43d1
rizdannn <rafifprastana@gmail.com>
2026-05-16 21:44:12 +0700

    Class Tugas Node Queue

---
+++ jobsheet 11/Tugas/NodeQueue22.java
@@ -0,0 +1,9 @@
+public class NodeQueue22 {
+    MahasiswaQ22 data;
+    NodeQueue22 next;
+
+    public NodeQueue22(MahasiswaQ22 data) {
+        this.data = data;
+        this.next = null;
+    }
+}
