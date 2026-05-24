d053d5ca7746ab057622bed830e58c4ec3ae3ccc
rizdannn <rafifprastana@gmail.com>
2026-05-24 17:34:21 +0700

    Class Pesanan

---
+++ case method 2/cm02/Pesanan22.java
@@ -0,0 +1,13 @@
+public class Pesanan22 {
+    int kodePesanan;
+    String namaPesanan;
+    int harga;
+    String namaPembeli;
+
+    public Pesanan22(int kodePesanan, String namaPesanan, int harga, String namaPembeli) {
+        this.kodePesanan = kodePesanan;
+        this.namaPesanan = namaPesanan;
+        this.harga = harga;
+        this.namaPembeli = namaPembeli;
+    }
+}
