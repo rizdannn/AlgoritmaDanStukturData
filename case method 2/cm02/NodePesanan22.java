6bf0e7e6018af6e7e63bf5df604eda41e829d2ad
rizdannn <rafifprastana@gmail.com>
2026-05-24 17:35:05 +0700

    Class Node Pesanan

---
+++ case method 2/cm02/NodePesanan22.java
@@ -0,0 +1,11 @@
+public class NodePesanan22 {
+    Pesanan22 data;
+    NodePesanan22 next;
+    NodePesanan22 prev;
+
+    public NodePesanan22(Pesanan22 data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
