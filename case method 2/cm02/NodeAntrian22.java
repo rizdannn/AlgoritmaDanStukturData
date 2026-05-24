0bac9be8dde38d4b4e7e90ac7d6ab1a80493d6fd
rizdannn <rafifprastana@gmail.com>
2026-05-24 17:34:43 +0700

    Class Antrian

---
+++ case method 2/cm02/NodeAntrian22.java
@@ -0,0 +1,11 @@
+public class NodeAntrian22 {
+    Pembeli22 data;
+    NodeAntrian22 next;
+    NodeAntrian22 prev;
+
+    public NodeAntrian22(Pembeli22 data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
