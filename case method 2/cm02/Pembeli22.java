90f8fba65e766bcd369c269181e84f0d814eeb9f
rizdannn <rafifprastana@gmail.com>
2026-05-24 17:34:03 +0700

    Class Pembeli

---
+++ case method 2/cm02/Pembeli22.java
@@ -0,0 +1,11 @@
+public class Pembeli22 {
+    int noAntrian;
+    String namaPembeli;
+    String noHp;
+
+    public Pembeli22(int noAntrian, String namaPembeli, String noHp) {
+        this.noAntrian = noAntrian;
+        this.namaPembeli = namaPembeli;
+        this.noHp = noHp;
+    }
+}
